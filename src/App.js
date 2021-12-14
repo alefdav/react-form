c94b8d4ec90edfc13340084c3903fe56e39c0bd9
alefdav <alefdav@gmail.com>
2021-12-14 20:01:14 -0300

    Form

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Container, Form, FloatingLabel } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App d-flex align-items-center h-100">
+      <Container>
+        <Form className="w-25 m-auto text-center">
+        <FloatingLabel controlId="floatingInput" label="Nome" className="mb-3">
+            <Form.Control type="name" placeholder="Nome"/>
+        </FloatingLabel>
+          
+          <FloatingLabel controlId="floatingInput" label="Endereço" className="mb-3">
+            <Form.Control type="adress" placeholder="Endereço" />
+          </FloatingLabel>
+          
+          <FloatingLabel controlId="floatingInput" label="Telefone" className="mb-3">
+            <Form.Control type="tel" placeholder="Telefone" />
+          </FloatingLabel>
+          
+          <FloatingLabel controlId="floatingInput" label="E-mail" className="mb-3">
+            <Form.Control type="email" placeholder="E-mail" />
+          </FloatingLabel>
+          
+          <FloatingLabel controlId="floatingInput" label="Data de nascimento" className="mb-3">
+            <Form.Control type="date" placeholder="Data de nascimento" />
+          </FloatingLabel>
+
+          <Button variant="dark" size="lg" type="submit" className="w-100">
+            Enviar
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
+}
+
+export default App;

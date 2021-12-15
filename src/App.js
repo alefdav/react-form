a9551f328e7bd3fff9beb24c2008dda43a055f2a
alefdav <alefdav@gmail.com>
2021-12-15 12:01:43 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,63 +1,60 @@
 import './App.css';
-import * as yup from 'yup';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Container, Form, FloatingLabel } from 'react-bootstrap';
+import { Button, Container, Form, FloatingLabel, Row, Col} from 'react-bootstrap';
+import {useForm} from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-import {formSchema} from './Validations/formValidation';
+import { formSchema } from './Validations/formValidation';
 
 
 function App() {
-  
-  const createForm = async (event) =>{
-    event.preventDefault();
-    
-    let formData = {
-      name: event.target[0].value,
-      adress: event.target[1].value,
-      tel: event.target[2].value,
-      email: event.target[3].value,
-      date: event.target[4].value
-    };
 
-    const isValid = await formSchema.isValid(formData);
+  const{register , handleSubmit, formState: {errors}} = useForm({
+    resolver: yupResolver(formSchema)
+  });
 
-    if(isValid){
-      alert('Mensagem Enviada');
-    }else{
-      alert('Erro');
-    }
-  }
+  const createForm = infos => console.log(infos);
 
   return (
     <div className="App d-flex align-items-center h-100">
-      <Container>
-        <Form className="w-25 m-auto text-center" onSubmit={createForm}>
-            <FloatingLabel controlId="" label="Nome" className="mb-3">
-              <Form.Control type="name" placeholder="Nome"/>
-            </FloatingLabel>
-            
-            <FloatingLabel controlId="" label="Endereço" className="mb-3">
-              <Form.Control type="adress" placeholder="Endereço"/>
-            </FloatingLabel>
-            
-            <FloatingLabel controlId="" label="Telefone" className="mb-3">
-              <Form.Control type="tel" placeholder="Telefone" />
-            </FloatingLabel>
-            
-            <FloatingLabel controlId="" label="E-mail" className="mb-3">
-              <Form.Control type="email" placeholder="E-mail" />
-            </FloatingLabel>
-            
-            <FloatingLabel controlId="" label="Data de nascimento" className="mb-3">
-              <Form.Control type="date" placeholder="Data de nascimento" />
-            </FloatingLabel>
+      <Container fluid>
+        <Row className="justify-content-center">
+          <Col xl={3} sm={6}>
+            <Form className="m-auto text-center" onSubmit={handleSubmit(createForm)}>
+              <FloatingLabel controlId="" label="Nome" className="mb-3">
+                <Form.Control type="text" name="nome" placeholder="(Nome)" {...register("nome")} />
+                  <p className='error-message'>{errors.nome?.message}</p>
+              </FloatingLabel>
 
-            <Button variant="dark" size="lg" type="submit" className="w-100">
-              Enviar
-            </Button>
-          </Form>
+              <FloatingLabel controlId="" label="Endereço" className="mb-3">
+                <Form.Control type="endereco" name="endereco" placeholder="Endereço" {...register("endereco")}/>
+                  <p className='error-message'>{errors.endereco?.message}</p>
+              </FloatingLabel>
+
+              <FloatingLabel controlId="" label="Telefone" className="mb-3">
+                <Form.Control className="inputNumber" type="number" name="numero" placeholder="Telefone" {...register("numero")}/>
+                  <p className='error-message'>{errors.numero?.message}</p>
+              </FloatingLabel>
+
+              <FloatingLabel controlId="" label="E-mail" className="mb-3">
+                <Form.Control name="email" placeholder="E-mail" {...register("email")}/>
+                  <p className='error-message'>{errors.email?.message}</p>
+              </FloatingLabel>
+
+              <FloatingLabel controlId="" label="Data de nascimento" className="mb-3">
+                <Form.Control type='date' data-date-format="DD MMMM YYYY" name="data" placeholder="Data de nascimento" {...register("data")}/>
+                  <p className='error-message'>{errors.data?.message}</p>
+              </FloatingLabel>
+
+              <Button variant="dark" size="lg" type="submit" className="w-100">
+                Enviar
+              </Button>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     </div>
+
   );
 }
 
